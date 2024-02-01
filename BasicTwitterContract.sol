f58c71819dcde8f277bac3995ebd4f9450a92f86
Hotboyy_x <asulewonbamiyemi@gmail.com>
2024-02-01 03:29:57 -0800

    Update BasicTwitterContract.sol

--- BasicTwitterContract.sol
+++ BasicTwitterContract.sol
@@ -1,1 +1,18 @@
+ // SPDX-License-Identifier: MIT
+
+ pragma solidity ^0.8.18;
+
+contract Twitter {
+        mapping(address => string) public tweets;
+
+        function createTweet(string memory _tweet) public {
+            tweets[msg.sender] = _tweet;
+        }
+
+    function getTweet (address _owner) public view returns(string memory ) {
+        return tweets[_owner];
+
+    }
+
+}
 
