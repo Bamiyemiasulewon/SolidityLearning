fd8b61e6664a31324481fe1a5e9c84c802fc3970
Hotboyy_x <asulewonbamiyemi@gmail.com>
2024-02-01 08:19:24 -0800

    Update BasicTwitterContract.sol

--- BasicTwitterContract.sol
+++ BasicTwitterContract.sol
@@ -3,15 +3,18 @@
  pragma solidity ^0.8.18;
 
 contract Twitter {
-        mapping(address => string) public tweets;
+        mapping(address => string[]) public tweets;
 
         function createTweet(string memory _tweet) public {
-            tweets[msg.sender] = _tweet;
+            tweets[msg.sender].push(_tweet);
         }
 
-    function getTweet (address _owner) public view returns(string memory ) {
-        return tweets[_owner];
+    function getTweet (address _owner, uint _i) public view returns(string memory ) {
+        return tweets[_owner][_i];
 
+    }
+    function getAllTweet(address _owner) public view returns(string[] memory) {
+        return tweets[_owner];
     }
 
 }
